363662c22a4e21f94b9777d6008012fba6f11885
yana nurika <72685243+yana-nuri@users.noreply.github.com>
2020-10-18 20:59:04 +0700

    Create LATIHAN 5.py

---
+++ PRATIKUM 2/LATIHAN 5/LATIHAN 5.py
@@ -0,0 +1,9 @@
+# Program untuk membuat 'diagram' jumlah siswa
+print('============== Program Membuat Diagram Jumlah Siswa =============='+'\n')
+lakiLaki = int(input('Berapa jumlah mahasiswa laki-laki di jurusan PTIK UNS ?'))
+perempuan = int(input('Berapa jumlah mahasiswa perempuan di jurusan PTIK UNS ?'))
+lk = int(lakiLaki/10)
+pr = int(perempuan/10)
+print('=============='+'\n')
+print('Laki-laki = '+'*' * lk)
+print('Perempuan = '+'*' * pr)
