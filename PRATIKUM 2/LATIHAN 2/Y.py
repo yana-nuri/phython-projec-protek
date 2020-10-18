97e169a8eabe5c009cecd53fd8167e0265e01aad
yana nurika <72685243+yana-nuri@users.noreply.github.com>
2020-10-18 20:58:07 +0700

    Create Y.py

---
+++ PRATIKUM 2/LATIHAN 2/Y.py
@@ -0,0 +1,7 @@
+# Variable assignment
+jarak = 795
+konsumsiBbm = 12
+# Operasi penghitungan
+hasilLiter = jarak/konsumsiBbm
+# Tampilkan hasil
+print('Total hasil liter bensin yang diperlukan untuk perjalanan : ' + str(hasilLiter))
