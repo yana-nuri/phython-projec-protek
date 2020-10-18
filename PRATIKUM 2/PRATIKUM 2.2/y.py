9878c1f2a548605087b21187ab12c4874d6bfc2d
yana nurika <72685243+yana-nuri@users.noreply.github.com>
2020-10-18 21:00:38 +0700

    Create y.py

---
+++ PRATIKUM 2/PRATIKUM 2.2/y.py
@@ -0,0 +1,202 @@
+# %
+# %ff
+x = 3.5
+print(type(x))
+y = 1.5
+print(type(y))
+print(type(x%y))
+
+# %fi
+x = 4.5
+print(type(x))
+y = 2
+print(type(y))
+print(type(x%y))
+
+# %if
+x = 4
+print(type(x))
+y = 1.5
+print(type(y))
+print(type(x%y))
+
+# %ii
+x = 3
+print(type(x))
+y = 2
+print(type(y))
+print(type(x%y))
+
+# **
+# **ff
+x = 0.5
+print(type(x))
+y = 0.5
+print(type(y))
+print(type(x**y))
+
+# **fi
+x = 0.5
+print(type(x))
+y = 2
+print(type(y))
+print(type(x**y))
+
+# **if
+x = 2
+print(type(x))
+y = 0.5
+print(type(y))
+print(type(x**y))
+
+# **ii
+x = 2
+print(type(x))
+y = 2
+print(type(y))
+print(type(x**y))
+
+# *
+# *ff
+x = 3.5
+print(type(x))
+y = 2.5
+print(type(y))
+print(type(x*y))
+
+#*fi
+x = 3.5
+print(type(x))
+y = 2
+print(type(y))
+print(type(x*y))
+
+#*if
+x = 2
+print(type(x))
+y = 2.5
+print(type(y))
+print(type(x*y))
+
+#*ii
+x = 2
+print(type(x))
+y = 2
+print(type(y))
+print(type(x*y))
+
+# +
+# +ff
+x = 3.5
+print(type(x))
+y = 5.5
+print(type(y))
+print(type(x+y))
+
+# +fi
+x = 3.5
+print(type(x))
+y = 5
+print(type(y))
+print(type(x+y))
+
+# +if
+x = 2
+print(type(x))
+y = 4.5
+print(type(y))
+print(type(x+y))
+
+# +ii
+x = 2
+print(type(x))
+y = 4
+print(type(y))
+print(type(x+y))
+
+# -
+# -ff
+x = 5.5
+print(type(x))
+y = 3.5
+print(type(y))
+print(type(x-y))
+
+# -fi
+x = 5.5
+print(type(x))
+y = 3
+print(type(y))
+print(type(x-y))
+
+# -if
+x = 4
+print(type(x))
+y = 2.5
+print(type(y))
+print(type(x-y))
+
+# -ii
+x = 4
+print(type(x))
+y = 2
+print(type(y))
+print(type(x-y))
+
+# :
+# :ff
+x = 3.3
+print(type(x))
+y = 3.3
+print(type(y))
+print(type(x/y))
+
+# :fi
+x = 3.3
+print(type(x))
+y = 3
+print(type(y))
+print(type(x/y))
+
+# :if
+x = 3
+print(type(x))
+y = 1.5
+print(type(y))
+print(type(x/y))
+
+# /ii
+x = 3
+print(type(x))
+y = 2
+print(type(y))
+print(type(x/y))
+
+# //
+# //ff
+x = 3.3
+print(type(x))
+y = 2.2
+print(type(y))
+print(type(x//y))
+
+# //fi
+x = 2.2
+print(type(x))
+y = 2
+print(type(y))
+print(type(x//y))
+
+# //if
+x = 3
+print(type(x))
+y = 1.5
+print(type(y))
+print(type(x//y))
+
+# //ii
+x = 3
+print(type(x))
+y = 1
+print(type(y))
+print(type(x//y))
