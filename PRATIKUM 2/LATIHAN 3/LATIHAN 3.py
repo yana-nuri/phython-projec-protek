3dff4cad59a47baa22fa3bc0781149e0052c096d
yana nurika <72685243+yana-nuri@users.noreply.github.com>
2020-10-18 20:58:21 +0700

    Create LATIHAN 3.py

---
+++ PRATIKUM 2/LATIHAN 3/LATIHAN 3.py
@@ -0,0 +1,8 @@
+# Variable assignment
+jarak = 795
+konsumsiBbm = 12
+kapasitasTangki = 50
+# Operasi penghitungan
+hasilFrekuensi = jarak/konsumsiBbm/kapasitasTangki
+# Tampilkan hasil
+print('Total frekuensi pak budi mengisi bensin sampai selesai perjalanan : ' + str(hasilFrekuensi))
