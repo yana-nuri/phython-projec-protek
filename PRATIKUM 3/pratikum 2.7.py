63a262471f7803fb91f7a1dfd5e821daf90ac4fd
yana nurika <72685243+yana-nuri@users.noreply.github.com>
2020-11-01 20:31:30 +0700

    Create pratikum 2.7.py

---
+++ PRATIKUM 3/pratikum 2.7.py
@@ -0,0 +1,9 @@
+from random import randint
+sum = 0
+while True:
+    bil = randint(0, 10)
+    print(bil)
+    sum = sum + 1
+    if bil == 5:
+        break
+print('jumlah perulangan : ' + str(sum))
