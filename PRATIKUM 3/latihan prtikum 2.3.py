04f5bf64d7c7ed94b8e16c580dd3481eae6b3150
yana nurika <72685243+yana-nuri@users.noreply.github.com>
2020-11-01 20:30:10 +0700

    Create latihan prtikum 2.3.py

---
+++ PRATIKUM 3/latihan prtikum 2.3.py
@@ -0,0 +1,10 @@
+k = 0
+i = 1
+sb = 0
+while (i <= 100):
+    print(i)
+    sb = sb + i
+    i = i + 2
+    k = k + 1
+print('banyaknya bilangan ganjil : ' + str(k))
+print('jumlah seluruh bilangan : ' + str(sb))
