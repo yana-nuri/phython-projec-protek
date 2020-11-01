9af210d7053ac5a857f53ed8a1e056109aa2bbe3
yana nurika <72685243+yana-nuri@users.noreply.github.com>
2020-11-01 20:32:30 +0700

    Create python if 1.5py.py

---
+++ PRATIKUM 3/python if 1.5py.py
@@ -0,0 +1,10 @@
+a = -2
+b =-7
+if (a > 0) and (b > 0):
+    print ("keduanya positif")
+elif (a < 0) and (b < 0):
+    print ("keduanya negatif")
+elif (a > 0) or (b > 0):
+    print ("salah satunya positif")
+elif (a < 0) or (a < 0):
+    print ("salah satunya negatif")
