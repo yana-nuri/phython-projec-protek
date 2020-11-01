8c3f4a9c9a6c831d65fe4e6c28c6a1a1fc7655c3
yana nurika <72685243+yana-nuri@users.noreply.github.com>
2020-11-01 22:07:32 +0700

    Create python if 1.8py.py

---
+++ PRATIKUM 3/python if 1.8py.py
@@ -0,0 +1,16 @@
+a = -2
+b = 9
+if (a > 0) and (b > 0):
+    print('keduanya positif')
+else:
+    print('keduanya tidak positif')
+
+a = -2
+b = 9
+if (a > 0):
+    if(b > 0):
+        print('keduanya positif')
+    else:
+        print('keduanya tidak positif')
+else:
+        print('keduanya tidak positif')
