a2e5be89e2619173d25d6428510fd288f2df8e22
yana nurika <72685243+yana-nuri@users.noreply.github.com>
2020-11-09 20:07:33 +0700

    Create CONTOH PRATIKUM 2.4.py

---
+++ PRATIKUM 4/CONTOH PRATIKUM 2.4.py
@@ -0,0 +1,12 @@
+def luasSegitiga2(a,t):
+    luas = a*t/2
+    print('Luas segitiga dg alas',a,
+          'dan tinggi',t,
+          'adalah',luas)
+
+alas = 10
+tinggi = 20
+alas1 = 15
+tinggi1 = 45
+luasSegitiga2(alas,tinggi)
+luasSegitiga2(alas,tinggi)
