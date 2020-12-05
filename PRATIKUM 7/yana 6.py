598cd65bbb9803879ea14b0509f60140279e4c1c
yana nurika <72685243+yana-nuri@users.noreply.github.com>
2020-12-05 20:52:27 +0700

    Create yana 6.py

---
+++ PRATIKUM 7/yana 6.py
@@ -0,0 +1,10 @@
+print ("=========================YANA NURIKA============================")
+print ("===========================K3520079=============================")
+def sortStringByChar(data):
+    data.sort(reverse=True)
+    return data
+
+data = ['apel','rambutan','jeruk']
+dataSorted = sortStringByChar(data)
+
+print("data baru : ", dataSorted)
