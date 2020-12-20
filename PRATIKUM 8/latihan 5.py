cbfdb678746c299afb2113bfbf1d4a3d7f10b086
yana nurika <72685243+yana-nuri@users.noreply.github.com>
2020-12-21 00:03:53 +0700

    Create latihan 5.py

---
+++ PRATIKUM 8/latihan 5.py
@@ -0,0 +1,18 @@
+print("-------------------YANA NURIKA-----------------")
+print("---------------------K3520079------------------")
+
+nilai = [{'nim' : 'A01', 'nama' : 'Agustina', 'mid' : 50, 'uas' : 80},
+         {'nim' : 'A02', 'nama' : 'Budi', 'mid' : 40, 'uas' : 90},
+         {'nim' : 'A03', 'nama' : 'Chicha', 'mid' : 100, 'uas' : 50},
+         {'nim' : 'A04', 'nama' : 'Donna', 'mid' : 20, 'uas' : 100},
+         {'nim' : 'A05', 'nama' : 'Fatimah', 'mid' : 70, 'uas' : 100}]
+
+print("=" * 41)
+print("NIM".ljust(8), "NAMA".ljust(8), "N.MID".rjust(8), "N.UAS".rjust(8))
+print("-" * 41)
+
+for x in nilai :
+          print(x['nim'].ljust(8), x['nama'].ljust(8), str(x['mid']).rjust(8), str(x['uas']).rjust(8))
+
+
+
