068e37715e5c440d901642673f78c22d3d83fbfc
yana nurika <72685243+yana-nuri@users.noreply.github.com>
2021-01-11 21:17:55 +0700

    Create projec3.py

---
+++ UAS SEMESTER 1/projec3.py
@@ -0,0 +1,39 @@
+print("-------------------YANA NURIKA-----------------")
+print("---------------------K3520079------------------")
+
+from datetime import *
+import projec1
+
+
+fileTeks = open('dataDiriPeminjamanBuku.txt', 'r')
+isiTeks = fileTeks.readlines()
+kodeBuku = input('Masukkan Kode Member : ')
+
+for x in range(len(isiTeks)) :
+    if (kodeBuku in isiTeks[x]) :
+        membagi = isiTeks[x].split('|')
+        statusBuku = 'ada'
+        break
+    else :
+        statusBuku = 'tidak ada'
+        continue
+
+if (statusBuku == 'ada'):
+    print('Data Peminjam Buku')
+    print('Kode Member                      : ', membagi[0])
+    print('Nama Member                      : ', membagi[1])
+    print('Judul Buku                       : ', membagi[2])
+    print('Tanggal Mulai Peminjaman         : ', membagi[3])
+    print('Tangal Pengambilan Maksimal      : ', membagi[4])
+    print('Tanggal Pengembalian Buku        : ', datetime.date(datetime.now()))
+    telatPengembalian = projec1. diffDate(membagi[4])
+    dendaTelat = 2000 * abs(telatPengembalian)
+
+    if(telatPengembalian >= 0 ) :
+        print('Terlambat : 0 hari')
+        print('Denda     : 0')
+    else :
+        print('Terlambat : ', abs(telatPengembalian))
+        print('Denda     : ', dendaTelat)
+else :
+    print('Peminjaman Buku Tidak Ada')
