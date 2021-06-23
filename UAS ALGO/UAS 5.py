f4bd7672d8ac000d0ee207bb9ff918c6d010fdfa
yana nurika <72685243+yana-nuri@users.noreply.github.com>
2021-06-23 22:18:23 +0700

    Create UAS 5.py

---
+++ UAS ALGO/UAS 5.py
@@ -0,0 +1,9 @@
+data = [21,39,23,25,49,41,20,21,45,30,25,36]
+
+datapertama = 25
+
+for i in range(len(data)) :
+    if (data[i] == datapertama) :
+        break
+
+print ("untuk posisi yang sudah ditemukan dari data ", datapertama, " pada index ke ", i)
