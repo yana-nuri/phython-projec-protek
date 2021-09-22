e04a6354598659fe7b1e19fa42cd80fbfa53a627
yana nurika <72685243+yana-nuri@users.noreply.github.com>
2021-09-22 21:52:42 +0700

    Create MyNewClass.java

---
+++ mypackage/MyNewClass.java
@@ -0,0 +1,10 @@
+package mypackage;
+public class MyNewClass {
+    public static void main(String[] args){
+        OperasiBilangan op1 = new OperasiBilangan();
+        op1.bilPertama = 50;
+        op1.bilKedua = 60;
+        op1.hitungPenjumlahan();
+        op1.hitungPengurangan();
+    }
+}
