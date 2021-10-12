417e59223b57d0777de7a03d1de60a50e090eee1
yana nurika <72685243+yana-nuri@users.noreply.github.com>
2021-10-12 16:46:09 +0700

    Create Segitiga.java

---
+++ ProjectBangunnDatar2/src/projectbangunndatar2/Segitiga.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectbangunndatar2;
+
+/**
+ *
+ * @author Acer
+ */
+public class Segitiga {
+     static double sisiMiring(double a, double b) {
+        return (Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2)));
+    }
+    double hitungLuasS(int alas, int tinggi) {
+        return ((alas * tinggi) / 2);
+    }
+    double hitungLuasS(int alas, double tinggi) {
+        return ((alas * tinggi) / 2);
+    }
+    double hitungLuasS(double alas, int tinggi) {
+        return ((alas * tinggi) / 2);
+    }
+    double hitungLuasS(double alas, double tinggi) {
+        return ((alas * tinggi) / 2);
+    } 
+    double hitungKelilingS(int alas, int tinggi) {
+        double SisiMiring = sisiMiring(alas, tinggi);
+        return (alas + tinggi + SisiMiring);
+    } 
+    double hitungKelilingS(int alas, double tinggi) {
+        double SisiMiring = sisiMiring(alas, tinggi);
+        return (alas + tinggi + SisiMiring);
+    } 
+    double hitungKelilingS(double alas, int tinggi) {
+        double SisiMiring = sisiMiring(alas, tinggi);
+        return (alas + tinggi + SisiMiring);
+    } 
+    double hitungKelilingS(double alas, double tinggi) {
+        double SisiMiring = sisiMiring(alas, tinggi);
+        return (alas + tinggi + SisiMiring);
+    }
+}
