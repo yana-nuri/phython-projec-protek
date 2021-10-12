86b1419ecd233a31dc740ebbc3e233ef9280106e
yana nurika <72685243+yana-nuri@users.noreply.github.com>
2021-10-12 16:45:49 +0700

    Create PersegiPanjang.java

---
+++ ProjectBangunnDatar2/src/projectbangunndatar2/PersegiPanjang.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectbangunndatar2;
+
+/**
+ *
+ * @author Acer
+ */
+public class PersegiPanjang {
+    int hitungLuasPP(int panjang, int lebar) {
+        return (panjang * lebar);
+    } 
+    double hitungLuasPP(int panjang, double lebar) {
+        return (panjang * lebar);
+    } 
+    double hitungLuasPP(double panjang, int lebar) {
+        return (panjang * lebar);
+    } 
+    double hitungLuasPP(double panjang, double lebar) {
+        return (panjang * lebar);
+    } 
+    int hitungKelilingPP(int panjang, int lebar) {
+        return (2 * (panjang + lebar));
+    } 
+    double hitungKelilingPP(int panjang, double lebar) {
+        return (2 * (panjang + lebar));
+    } 
+    double hitungKelilingPP(double panjang, int lebar) {
+        return (2 * (panjang + lebar));
+    } 
+    double hitungKelilingPP(double panjang, double lebar) {
+        return (2 * (panjang + lebar));
+    }
+}
