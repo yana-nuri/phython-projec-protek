52ac1197f1a37905b55f0bb416787776b42b726e
yana nurika <72685243+yana-nuri@users.noreply.github.com>
2021-11-23 22:12:13 +0700

    Create SilverMember.java

---
+++ RentalConsole/src/rentalconsole/SilverMember.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rentalconsole;
+
+/**
+ *
+ * @author Acer
+ */
+public class SilverMember extends Others {
+
+    protected int rentCost, point, disc, tempCost, discountAmount, totalCost;
+
+    SilverMember() {
+        this.rentCost = 25000;
+        this.point = 1;
+        this.disc = 1;
+    }
+    
+    protected int costAmount(int rentalDuration) {
+        
+        tempCost = rentalDuration * rentCost;
+        discountAmount = (tempCost * disc) / 100;
+        totalCost = tempCost - discountAmount;
+        
+        return totalCost;
+    }
+    
+    protected int getPoint(int rentalDuration) {
+        
+        int totalPoint = rentalDuration * point;
+        return totalPoint;
+    }
+    
+} 
